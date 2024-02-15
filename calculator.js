94f5628b9c6dda1b974d159fa3bad3a685734cdb
Saaideh-Osama <saaidehosama@gmail.com>
2024-02-15 12:12:51 +0300

    calculator body

---
+++ calculator.js
@@ -0,0 +1,43 @@
+document.getElementById('result').value = " ";
+function add1(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='1';
+}
+function add2(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='2';
+}
+function add3(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='3';
+}
+function add4(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='4';
+}
+function add5(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='5';
+}
+function add6(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='6';
+}
+function add7(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='7';
+}
+function add8(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='8';
+}
+function add9(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='9';
+}
+function addmul(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='*';
+}
+function adddiv(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='/';
+}
+function addadd(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='+';
+}
+function addsub(){
+    document.getElementById('result').innerText = document.getElementById('result').value+='-';
+}
+function Clear(){
+    document.getElementById('result').innerText = " ";
+}
